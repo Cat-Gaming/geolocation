34a8ffd29290313911026eb92a55ff9bbb4bd628
Tyler Wendorff <tylerwendorff@yahoo.com>
2020-09-20 13:08:58 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Get Location
+if (navigator.geolocation)
+{
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+// make map
+function showPosition(position)
+{
+    var mapOptions = {
+        center: [position.coords.latitude, position.coords.longitude],
+        zoom: 12
+    }
+    
+    var map = new L.map('map', mapOptions);
+    var layer = new L.TileLayer(`http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`);
+    map.addLayer(layer);
+}
